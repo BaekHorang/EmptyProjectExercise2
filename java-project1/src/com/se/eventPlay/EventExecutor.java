c02efde87aef98df29468c1e3dda2e20b94b42a9
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-03-09 23:48:08 +0900

    Update EventExecutor

---
+++ java-project1/src/com/se/eventPlay/EventExecutor.java
@@ -0,0 +1,15 @@
+package com.se.eventPlay;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+/**
+ * Created by user on 2016-03-09.
+ */
+public class EventExecutor extends UiAutomatorTestCase{
+
+    public static void main(String []args)
+    {
+        System.out.println("Hello IntlliJ!");
+        System.out.println("Hello IntelliJ2 for update");
+    }
+}
